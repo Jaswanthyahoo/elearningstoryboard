94438f0bbfa3eab4f423fd9ca4f621c38e5cf738
Jaswanthyahoo <jaswanth19_mca@yahoo.co.in>
2017-04-03 09:07:14 +0530

    scroll and answer in timeline

--- js/main.js
+++ js/main.js
@@ -134,3 +134,27 @@ searchQuestionsResults = {
         $('#alertPostAnswer').removeClass('clshidden').addClass('clsshow');
     }
 }
+
+//Scroll to top Js
+if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
+//Scroll to top Js
